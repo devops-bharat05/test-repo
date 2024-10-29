990853316cc6fa05af57247201a8d779197a7063
Bharat Bhushan <116905352+devops-bharat05@users.noreply.github.com>
2024-10-29 20:49:11 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+import time
+app= Flask(__name__)
+
+@app.route('/name')
+def get_name():
+	return 'AWS Masterchef'
+
+@app.route('/version')
+def get_version():
+	return'v1.0.0'
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',port=5000)
+	
